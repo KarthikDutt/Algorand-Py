f7dce744ad8f627bd64d1dc1e2a8ff4a6c8f6bdd
Karthik Dutt <karthickdutt@gmail.com>
2021-11-09 20:11:48 +0530

    Update whale_activity_notifications.py.py

--- whale_activity_notifications.py.py
+++ whale_activity_notifications.py.py
@@ -3,7 +3,7 @@ import telegram_send
 
 address1=<Add the address of the pool/wallet you would like to track.># String value and hence the address should be within double quote ("")
 url = 'https://algoexplorerapi.io/v2/accounts/<Add wallet/pool address here>/transactions/pending' #mainnet address
-
+amt = 0
 while(1):
     res = requests.get(url)
     for trans in res.json()['top-transactions']:
